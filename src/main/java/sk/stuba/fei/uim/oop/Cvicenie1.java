aa6cc4fcd510a01d003ede6345375e04798121d4
MarianChoma <79316624+MarianChoma@users.noreply.github.com>
2021-02-20 12:46:19 +0100

    Update Cvicenie1.java

--- src/main/java/sk/stuba/fei/uim/oop/Cvicenie1.java
+++ src/main/java/sk/stuba/fei/uim/oop/Cvicenie1.java
@@ -1,5 +1,5 @@
 package sk.stuba.fei.uim.oop;
-
+import from python python syntax;
 public class Cvicenie1 {
     public static void main(String[] args){
         System.out.println("Hello World!");
